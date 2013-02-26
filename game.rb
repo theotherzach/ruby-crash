73e7f14fb0c26086e5268be168510e7eb7bace48
TheOtherZach <briggszj@gmail.com>
2013-02-26 00:22:15 -0500

    Game::score is now calculating score

--- game.rb
+++ game.rb
@@ -1,17 +1,19 @@
 class Game
   def initialize
-    @score = 0
     @rolls = Array.new(22, 0)
     @current_roll = 0
   end
 
-  def roll(pins) # roll calculates score
-    @score += pins
+  def roll(pins)
     @rolls[@current_roll] = pins
     @current_roll += 1
   end
 
-  def score # score does not calculate score
-    @score
+  def score
+    score = 0
+    @rolls.each do |roll|
+      score += roll
+    end
+    score
   end
 end
