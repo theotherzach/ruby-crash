17fd0c6c0a6ff84fefe037f631bbdf6ce95cbd8a
TheOtherZach <briggszj@gmail.com>
2013-02-26 01:04:33 -0500

    moved score methods

--- game.rb
+++ game.rb
@@ -14,13 +14,13 @@ class Game
     frame_index = 0
     10.times do
       if @rolls[frame_index] == 10 # strike
-        score += 10 + @rolls[frame_index + 1] + @rolls[frame_index + 2]
+        score += strike_bonus(frame_index)
         frame_index += 1
       elsif spare?(frame_index)
-        score += 10 + @rolls[frame_index + 2]
+        score += spare_bonus(frame_index)
         frame_index += 2
       else
-        score += @rolls[frame_index] + @rolls[frame_index + 1]
+        score += sum_open_frame(frame_index)
         frame_index += 2
       end
     end
@@ -28,6 +28,18 @@ class Game
   end
 
   private
+  def sum_open_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
+  def spare_bonus(frame_index)
+    10 + @rolls[frame_index + 2]
+  end
+
+  def strike_bonus(frame_index)
+    10 + @rolls[frame_index + 1] + @rolls[frame_index + 2]
+  end
+
   def spare?(frame_index)
     (@rolls[frame_index] + @rolls[frame_index + 1]) == 10
   end
