562dad8a465f4d713b1f70d8d557ae3f50a03e30
TheOtherZach <briggszj@gmail.com>
2013-02-25 23:37:24 -0500

    creating game in setup

--- game_test.rb
+++ game_test.rb
@@ -2,15 +2,17 @@ require 'minitest/autorun'
 require_relative 'game'
 
 class GameTest < MiniTest::Unit::TestCase
+  def setup
+    @game = Game.new
+  end
+
   def test_gutter_game
-    game = Game.new
-    20.times { game.roll(0) }
-    assert_equal(0, game.score)
+    20.times { @game.roll(0) }
+    assert_equal(0, @game.score)
   end
 
   def test_all_ones
-    game = Game.new
-    20.times { game.roll(1) }
-    assert_equal(20, game.score)
+    20.times { @game.roll(1) }
+    assert_equal(20, @game.score)
   end
 end
