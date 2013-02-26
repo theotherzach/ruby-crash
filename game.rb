7c5c0b11ce2d35bdcb874891518754440a3aec4e
TheOtherZach <briggszj@gmail.com>
2013-02-26 00:15:53 -0500

    started to refactor score

--- game.rb
+++ game.rb
@@ -1,10 +1,14 @@
 class Game
   def initialize
     @score = 0
+    @rolls = Array.new(22, 0)
+    @current_roll = 0
   end
 
   def roll(pins) # roll calculates score
     @score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
 
   def score # score does not calculate score
