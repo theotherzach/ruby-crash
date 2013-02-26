e6eb8b6c1aafc2439eedf3307d16d975f20d0bed
TheOtherZach <briggszj@gmail.com>
2013-02-25 23:37:24 -0500

    creating game in setup

--- game_test.rb
+++ game_test.rb
@@ -3,15 +3,17 @@ require 'turn'
 require_relative 'game'
 
 class GameTest < MiniTest::Unit::TestCase
+  def setup
+    @game = Game.new
+  end
+
   def test_gutter_game
-    game = Game.new
-    20.times { game.roll(0) }
-    assert_equal(0, game.score)
+    20.times { @game.roll(0) }
+    assert_equal(0, @game.score)
   end
 
   def test_all_ones
-    game = Game.new
-    20.times { game.roll(1) }
-    assert_equal(20, game.score)
+    20.times { @game.roll(1) }
+    assert_equal(20, @game.score)
   end
 end
